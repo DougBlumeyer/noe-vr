201e2eec499213b98a1fc06321d2ed1630be8d9f
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-01-24 15:48:00 -0800

    factor out nudgeAcceleration

--- NoeScript.js
+++ NoeScript.js
@@ -13,9 +13,13 @@ function Start () {
 
 function nudgeAccelerations() {
 	for (var i = 0; i < 3; i++) {
-		translationAcceleration[i] = Mathf.Clamp(translationAcceleration[i] + randomValuePosOrNeg(1.0), -1.0, 1.0);
-		rotationAcceleration[i] = Mathf.Clamp(rotationAcceleration[i] + randomValuePosOrNeg(5.0), -10.0, 10.0);
+		nudgeAcceleration(translationAcceleration[i], 1.0, 1.0);
+		nudgeAcceleration(rotationAcceleration[i], 5.0, 10.0);
 	}
+}
+
+function nudgeAcceleration(el : float, scalar : float, clamp : float) {
+	el = Mathf.Clamp(el + randomValuePosOrNeg(scalar), -clamp, clamp);
 }
 
 function randomValuePosOrNeg(scalar : float) {
