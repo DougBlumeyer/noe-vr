6043070a3b7e9865f0b0271c97d521bafa40927c
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-01-24 15:36:40 -0800

    rename variables

--- NoeScript.js
+++ NoeScript.js
@@ -1,20 +1,20 @@
 ﻿#pragma strict
 
-var positionVector = new Vector3();
-var rotationVector = new Vector3();
+var translationAcceleration = new Vector3();
+var rotationAcceleration = new Vector3();
 for (var i = 0; i < 3; i++) {
-	positionVector[i] = randomValuePosOrNeg(1.0);
-	rotationVector[i] = randomValuePosOrNeg(10.0);
+	translationAcceleration[i] = randomValuePosOrNeg(1.0);
+	rotationAcceleration[i] = randomValuePosOrNeg(10.0);
 }
 
 function Start () {
-	InvokeRepeating("nudgeRotationRates", 0.0, 3.0);
+	InvokeRepeating("nudgeAccelerations", 0.0, 3.0);
 }
 
-function nudgeRotationRates() {
+function nudgeAccelerations() {
 	for (var i = 0; i < 3; i++) {
-		positionVector[i] = Mathf.Clamp(positionVector[i] + randomValuePosOrNeg(1.0), -1.0, 1.0);
-		rotationVector[i] = Mathf.Clamp(rotationVector[i] + randomValuePosOrNeg(5.0), -10.0, 10.0);
+		translationAcceleration[i] = Mathf.Clamp(translationAcceleration[i] + randomValuePosOrNeg(1.0), -1.0, 1.0);
+		rotationAcceleration[i] = Mathf.Clamp(rotationAcceleration[i] + randomValuePosOrNeg(5.0), -10.0, 10.0);
 	}
 }
 
@@ -24,6 +24,6 @@ function randomValuePosOrNeg(scalar : float) {
 
 function Update()
 {
-	transform.Rotate(rotationVector*Time.deltaTime);
-	transform.Translate(positionVector*Time.deltaTime);
+	transform.Rotate(rotationAcceleration*Time.deltaTime);
+	transform.Translate(translationAcceleration*Time.deltaTime);
 }
