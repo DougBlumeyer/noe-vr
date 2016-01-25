e084d279543eaaa2f7d1e7dc41aa6974a000c346
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2016-01-24 20:10:37 -0800

    gravity toward a home orientation and position

--- NoeScript.js
+++ NoeScript.js
@@ -14,8 +14,8 @@ for (var i = 0; i < 3; i++) {
 	rotationVelocityActual[i] = rotationVelocityTarget[i];
 }
 
-//var translationHome = new Vector3(0.0, 0.0, 0.0);
-//var rotationHome = new Vector3(0.0, 0.0, 0.0);
+var translationHome = new Vector3(0.0, 0.0, 0.0);
+var rotationHome = new Vector3(0.0, 0.0, 0.0);
 
 function Start () {
 	InvokeRepeating("nudgeVelocityTargets", 0.0, 0.5);
@@ -23,36 +23,34 @@ function Start () {
 
 function nudgeVelocityTargets() {
 	for (var i = 0; i < 3; i++) {
-		translationVelocityTarget[i] = nudgeTarget(translationVelocityTarget[i], 0.5, 1.0);
-		rotationVelocityTarget[i] = nudgeTarget(rotationVelocityTarget[i], 3.0, 4.0);
-		//nudge(translationVelocityTarget[i], 1.0, 3.0, translationHome[i], 0.6, this.transform.position[i]);
-		//nudge(rotationVelocityTarget[i], 3.0, 7.0, rotationHome[i], 0.1, this.transform.rotation[i]);
+		translationVelocityTarget[i] = nudgeRandomly(translationVelocityTarget[i], 0.1, 0.2);
+		rotationVelocityTarget[i] = nudgeRandomly(rotationVelocityTarget[i], 3.0, 4.0);
+
+		translationVelocityTarget[i] = nudgeTowardHome(translationVelocityTarget[i], translationHome[i], this.transform.position[i], 0.1);
+		rotationVelocityTarget[i] = nudgeTowardHome(rotationVelocityTarget[i], rotationHome[i], this.transform.rotation[i], 0.1);
 	}
 }
 
-//function nudge(number : float, scalar : float, clamp : float, home : float, adjustmentScalar : float, absolute : float) {
-	//number = Mathf.Clamp(number + randomValuePosOrNeg(scalar) + adjustmentTowardHome(absolute, home, adjustmentScalar), -clamp, clamp);
-function nudgeTarget(number : float, scalar : float, clamp : float) {
+function nudgeRandomly(number : float, scalar : float, clamp : float) {
 	return Mathf.Clamp(number + randomValuePosOrNeg(scalar), -clamp, clamp);
 }
 
-//function adjustmentTowardHome(number : float, home : float, scalar : float) {
-//	return (home - number) * scalar;
-//}
+function nudgeTowardHome(velocity : float, homeValue : float, currentValue : float, scalar : float) {
+	return velocity + (homeValue - currentValue) * scalar;
+}
 
 function randomValuePosOrNeg(scalar : float) {
 	return (Random.value * 2.0 - 1.0) * scalar;
 }
 
-function nudgeActual(actual : float, target : float, scalar : float) {
+function nudgeActualTowardTarget(actual : float, target : float, scalar : float) {
 	return actual + (target - actual) * scalar;
 }
 
-function Update()
-{
+function Update() {
 	for (var i = 0; i < 3; i++) {
-		rotationVelocityActual[i] = nudgeActual(rotationVelocityActual[i], rotationVelocityTarget[i], 0.02);
-		translationVelocityActual[i] = nudgeActual(translationVelocityActual[i], translationVelocityTarget[i], 0.05);
+		rotationVelocityActual[i] = nudgeActualTowardTarget(rotationVelocityActual[i], rotationVelocityTarget[i], 0.02);
+		translationVelocityActual[i] = nudgeActualTowardTarget(translationVelocityActual[i], translationVelocityTarget[i], 0.05);
 	}
 	transform.Translate(translationVelocityActual*Time.deltaTime);
 	transform.Rotate(rotationVelocityActual*Time.deltaTime);
